4c126a5001667664f8680823efd305a368716ebf
Tomasz Klim <github@tomaszklim.pl>
2016-11-17 22:35:22 +0100

    openstack support

--- add-samba-user.sh
+++ add-samba-user.sh
@@ -12,20 +12,19 @@ MINUID=1600
 MAXUID=1799
 
 
+type=`/opt/farm/scripts/config/detect-hostname-type.sh $2`
+
 if [ "$2" = "" ]; then
 	echo "usage: $0 <user> <samba-server[:port]> [backup-server[:port]]"
 	exit 1
 elif ! [[ $1 =~ ^[a-z0-9]+$ ]]; then
-	echo "error: parameter 1 not conforming user name format"
-	exit 1
-elif ! [[ $2 =~ ^[a-z0-9.-]+[.][a-z0-9]+([:][0-9]+)?$ ]]; then
-	echo "error: parameter 2 not conforming host name format"
+	echo "error: parameter $1 not conforming user name format"
 	exit 1
 elif [ -d /home/smb-$1 ]; then
 	echo "error: user $1 exists"
 	exit 1
-elif [ "`getent hosts $2`" = "" ]; then
-	echo "error: host $2 not found"
+elif [ "$type" != "hostname" ] && [ "$type" != "ip" ]; then
+	echo "error: parameter $2 not conforming hostname format, or given hostname is invalid"
 	exit 1
 fi
 
@@ -48,9 +47,10 @@ else
 fi
 
 if [ "$backupserver" != "" ] && [ "$backupserver" != "$sambaserver" ]; then
+	type=`/opt/farm/scripts/config/detect-hostname-type.sh $backupserver`
 
-	if ! [[ $backupserver =~ ^[a-z0-9.-]+[.][a-z0-9]+([:][0-9]+)?$ ]]; then
-		echo "error: parameter 3 not conforming host name format"
+	if [ "$type" != "hostname" ] && [ "$type" != "ip" ]; then
+		echo "error: parameter $3 not conforming hostname format, or given hostname is invalid"
 		exit 1
 	fi
 
@@ -60,11 +60,6 @@ if [ "$backupserver" != "" ] && [ "$backupserver" != "$sambaserver" ]; then
 	else
 		backuphost=$backupserver
 		backupport=22
-	fi
-
-	if [ "`getent hosts $backuphost`" = "" ]; then
-		echo "error: host $backuphost not found"
-		exit 1
 	fi
 fi
 
