26c726fc9e03dc6d91e5926fd3e2cb98f4885886
Tomasz Klim <github@tomaszklim.pl>
2017-10-20 16:00:59 +0200

    new sf-repos extension

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/opt/farm/scripts/setup/role.sh sf-farm-manager
+/opt/farm/scripts/setup/extension.sh sf-farm-manager
 
 ln -sf /opt/farm/ext/samba-manager/add-samba-user.sh /usr/local/bin/add-samba-user
