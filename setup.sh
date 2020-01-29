c9066b4f5e277313a0ecb019722cf05d83c3c37b
Tomasz Klim <github@tomaszklim.pl>
2020-01-29 23:25:08 +0100

    use the new method of handling /usr/local/bin aliases

--- setup.sh
+++ setup.sh
@@ -3,5 +3,3 @@
 /opt/farm/scripts/setup/extension.sh sf-net-utils
 /opt/farm/scripts/setup/extension.sh sf-passwd-utils
 /opt/farm/scripts/setup/extension.sh sf-farm-manager
-
-ln -sf /opt/farm/ext/samba-manager/add-samba-user.sh /usr/local/bin/add-samba-user
--- uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-if [ -h /usr/local/bin/add-samba-user ]; then
-	rm -f /usr/local/bin/add-samba-user
-fi
